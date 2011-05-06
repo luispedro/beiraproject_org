c32018c083cc266804a4effffd871dee264e0c57
Luis Pedro Coelho <lpc@cmu.edu>
2011-05-06 05:29:46 +0000

    ENH Slightly shorter text

--- signup/models.py
+++ signup/models.py
@@ -12,7 +12,7 @@ class Signup(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(blank=True, null=True)
     date = models.DateTimeField(default=datetime.now)
-    nr_guests = models.PositiveIntegerField(default=1, verbose_name='Including myself, my party will have these many people')
+    nr_guests = models.PositiveIntegerField(default=1, verbose_name='Including myself, I will bring these many people')
     attribution = models.CharField(max_length=2, choices=_Attribution_Choices, default='ME')
     attribute_to = models.CharField(max_length=255)
     donation = models.IntegerField(default=35, help_text="Recommended: $35 per person")
